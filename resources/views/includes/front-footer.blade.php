52a110978edd489f95c18c94023a0434646a377c
Gav <26879898+RillistikPete@users.noreply.github.com>
2026-09-04 10:56:08 -0400

    footer style and using resend for emails

--- resources/views/includes/front-footer.blade.php
+++ resources/views/includes/front-footer.blade.php
@@ -1,14 +1,14 @@
 
     <!-- Footer -->
-    <div class="container">
-        <footer>
+    <footer class="site-footer">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                <p>Copyright &copy; Codehack Blog 2019</p>
+                    <p>Copyright &copy; Codehack Blog 2019 - {{ now()->year }}</p>
                 </div>
             </div>
-        </footer>
-    </div>
+        </div>
+    </footer>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
