f567eff1b77e6ece3c3eef5ff739ab039dd82778
Gav <26879898+RillistikPete@users.noreply.github.com>
2026-09-04 12:40:54 -0400

    modified prod workflow to fix node section and artifact upload

--- resources/views/includes/front-footer.blade.php
+++ resources/views/includes/front-footer.blade.php
@@ -1,14 +1,14 @@
 
     <!-- Footer -->
-    <footer class="site-footer">
-        <div class="container">
+    <div class="container">
+        <footer class="site-footer">
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Codehack Blog 2019 - {{ now()->year }}</p>
+                    <p>Copyright &copy; Codehack Blog 2018 - {{ now()->year }}</p>
                 </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js"></script>
