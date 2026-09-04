686babc789638f9febec3fb951a8209d6c56e35e
Gav <26879898+RillistikPete@users.noreply.github.com>
2026-09-03 23:34:22 -0400

    Import URL facade in AppServiceProvider, bump ci version for  Node to 24

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
