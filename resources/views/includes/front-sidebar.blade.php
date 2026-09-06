f05b234cb7e741ca73817016f7cb1a1fa2f22c61
Gav <26879898+RillistikPete@users.noreply.github.com>
2026-09-06 10:03:13 -0400

    edit to front sidebar

--- resources/views/includes/front-sidebar.blade.php
+++ resources/views/includes/front-sidebar.blade.php
@@ -35,8 +35,8 @@
         <p>
             <img src="{{ asset('images/me-n-mari-zgz.jpg') }}" alt="Gavin Forrest" class="about-avatar">
             I'm Gavin, a full-stack developer working mainly in
-            <strong>C# / ASP.NET Core / Azure</strong> and <strong>PHP/Laravel</strong>,
-            with SQL, Postgres, Docker and AWS underneath.
+            <strong>C# / ASP.NET Core / Vue</strong> and <strong>PHP/Laravel</strong>,
+            with Azure, SQL, Postgres, Docker and AWS underneath.
         </p>
         <p>
             I write about the problems that don't have a clean answer online - things I've run into and my debugging
